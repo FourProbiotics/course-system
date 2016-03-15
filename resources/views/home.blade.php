c2950802c2c2227f9acf90a30c2fd08ade038133
陈然 <396147081@qq.com>
2016-03-15 15:41:10 +0800

    增加汉化文件，汉化系统

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">仪表盘</div>
 
                     <div class="panel-body">
-                        You are logged in!
+                        你已成功登录!
                     </div>
                 </div>
             </div>
