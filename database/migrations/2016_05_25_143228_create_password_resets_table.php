3cee856dbae9bcd38b5c0eaba22eb6fddb0fd005
陈然 <396147081@qq.com>
2016-05-25 22:33:49 +0800

    忘记加重置密码表了

---
+++ database/migrations/2016_05_25_143228_create_password_resets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasswordResetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('uid')->index()->comment = "用户id";
+            $table->string('token')->index();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('password_resets');
+    }
+}
